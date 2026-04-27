2641f78259caccdb1e0c25de4ba610bd20a8770f
hhhao <hhhao.vn@gmail.com>
2026-04-27 16:24:05 +0700

    feat: Add landing page

--- frontend/app/signin/page.tsx
+++ frontend/app/signin/page.tsx
@@ -1,18 +1,17 @@
 "use client"
 
 import { SigninForm } from "@/components/auth/signin-form"
-import { IconDatabase } from "@tabler/icons-react"
+import Image from "next/image"
+import Link from "next/link"
 
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <IconDatabase className="size-4" />
-          </div>
-          querizer
-        </a>
+      <div className="flex flex-col items-center w-full max-w-sm gap-6">
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/icon.svg" alt="querizer" width={28} height={28} className="rounded-md" />
+          <p className="font-semibold">querizer</p>
+        </Link>
         <SigninForm />
       </div>
     </div>
